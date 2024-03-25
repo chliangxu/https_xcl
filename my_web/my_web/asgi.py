f444edff352ff5d5f133d1a0791ec66745212b14
v_chliangxu(徐成亮) <v_chliangxu@tencent.com>
2024-03-25 09:57:09 +0800

    Django4.2.7

--- my_web/my_web/asgi.py
+++ my_web/my_web/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for http_xcl project.
+ASGI config for my_web project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- my_web/my_web/urls.py
+++ my_web/my_web/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for http_xcl project.
+URL configuration for my_web project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,16 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from app01 import views
+from django.urls import path,include
 
-
-# 在urlpatterns中加入path(‘hello/’, views.hello)，第一个元素是匹配的字符串，第二个元素为相对应的视图模块。
-# 也就是告诉django所有url/hello/ 的请求都是指向了views.hello 这个视图。hello前不需要加’/‘,因为域名的末尾一定会有’/‘。其中’^‘为严格前匹配,’,浏览器输入http://localhost:8000/hello/a/b 也是可以访问view.hello视图
-
+# 总路由
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("hello/", views.hello),
-    path("book/", views.test)
-    # path("publisher-polls/", include("polls.urls", namespace="publisher-polls")),
+    path("app01/", include("app01.urls")),
 ]
